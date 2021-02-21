a8664a24303dca5f0e3ff2333ab0fefa4deae350
Bernardo Mesko <bemesko@gmail.com>
2021-02-21 10:54:20 -0300

    Começo do ButtonSpawner

---
+++ Assets/Scripts/ButtonSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _pointButtonGameObject = default;
+
+    [SerializeField]
+    private Canvas _parentCanvas = default;
+
+    public void SpawnButton(){
+        Instantiate(_pointButtonGameObject);
+    }
+}
