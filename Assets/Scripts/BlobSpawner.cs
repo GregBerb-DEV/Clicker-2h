72c3517fccad02c88b822829f4a454ee91379f24
Bernardo Mesko <bemesko@gmail.com>
2021-02-27 20:45:34 -0300

    Spawna botões dentro do canvas

---
+++ Assets/Scripts/BlobSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _blobGameObject = default;
+    [SerializeField]
+    private RectTransform _parentCanvasTransform;
+
+    private void Start()
+    {
+        SpawnButton();
+    }
+
+    public void SpawnButton()
+    {
+        Instantiate(_blobGameObject, new Vector3(0, 0, 0), Quaternion.identity, _parentCanvasTransform);
+    }
+}
