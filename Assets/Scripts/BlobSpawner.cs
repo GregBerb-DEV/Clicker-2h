6774cf8d4b047ccc1ef1c380a61b297f8e87c6ca
Bernardo Mesko <bemesko@gmail.com>
2021-02-27 21:11:31 -0300

    Spawner vários botões em tempos aleatórios

--- Assets/Scripts/BlobSpawner.cs
+++ Assets/Scripts/BlobSpawner.cs
@@ -12,10 +12,26 @@ public class BlobSpawner : MonoBehaviour
     [SerializeField] private int _maxSpawnX;
     [SerializeField] private int _minSpawnY;
     [SerializeField] private int _maxSpawnY;
+    [SerializeField] private float _minSpawnTime;
+    [SerializeField] private float _maxSpawnTime;
+
+    private float Timer;
 
     private void Start()
     {
         SpawnButton();
+        Timer = Random.Range(_minSpawnTime, _maxSpawnTime);
+    }
+
+    private void Update()
+    {
+        if (Timer < Time.time)
+        {
+            SpawnButton();
+
+            Timer += Random.Range(_minSpawnTime, _maxSpawnTime);
+        }
+
     }
 
     public void SpawnButton()
@@ -23,7 +39,7 @@ public class BlobSpawner : MonoBehaviour
         int spawnX = Random.Range(_minSpawnX, _maxSpawnY);
         int spawnY = Random.Range(_minSpawnY, _maxSpawnY);
         Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0);
-        Debug.Log(spawnPosition);
+
         GameObject _instantiatedBlob = Instantiate(_blobGameObject, new Vector3(0, 0, 0), Quaternion.identity, _parentPanelTransform);
         _instantiatedBlob.GetComponent<RectTransform>().localPosition = spawnPosition;
     }
