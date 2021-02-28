fd932fbbe5de5c3c837d9f3e7cc51223c401a2d1
Bernardo Mesko <bemesko@gmail.com>
2021-02-27 22:00:50 -0300

    Destrói blobs quando clica ou quando passa muito tempo

--- Assets/Scripts/BlobScript.cs
+++ Assets/Scripts/BlobScript.cs
@@ -6,16 +6,26 @@ using UnityEngine.UI;
 public class BlobScript : MonoBehaviour
 {
     public static int _scoreIncrement = 1;
+    [SerializeField] private float _buttonLifespan = 3;
     private Button _button;
+    private float _startTime;
 
     void Start()
     {
         _button = gameObject.GetComponent<Button>();
+        _startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time >= _startTime + _buttonLifespan)
+            Destroy(gameObject);
     }
 
     public void UpdateScore(int _PlusHightScore)
     {
         LeftPanel.Instance.UpdateScore(_scoreIncrement);
+        Destroy(gameObject);
     }
 
 
