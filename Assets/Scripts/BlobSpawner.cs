f761291615c6770ae9b03a7ed55245bc456b8043
Bernardo Mesko <bemesko@gmail.com>
2021-02-27 21:00:32 -0300

    Spawna botão em posição aleatória

--- Assets/Scripts/BlobSpawner.cs
+++ Assets/Scripts/BlobSpawner.cs
@@ -7,7 +7,11 @@ public class BlobSpawner : MonoBehaviour
     [SerializeField]
     private GameObject _blobGameObject = default;
     [SerializeField]
-    private RectTransform _parentCanvasTransform;
+    private RectTransform _parentPanelTransform;
+    [SerializeField] private int _minSpawnX;
+    [SerializeField] private int _maxSpawnX;
+    [SerializeField] private int _minSpawnY;
+    [SerializeField] private int _maxSpawnY;
 
     private void Start()
     {
@@ -16,6 +20,11 @@ public class BlobSpawner : MonoBehaviour
 
     public void SpawnButton()
     {
-        Instantiate(_blobGameObject, new Vector3(0, 0, 0), Quaternion.identity, _parentCanvasTransform);
+        int spawnX = Random.Range(_minSpawnX, _maxSpawnY);
+        int spawnY = Random.Range(_minSpawnY, _maxSpawnY);
+        Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0);
+        Debug.Log(spawnPosition);
+        GameObject _instantiatedBlob = Instantiate(_blobGameObject, new Vector3(0, 0, 0), Quaternion.identity, _parentPanelTransform);
+        _instantiatedBlob.GetComponent<RectTransform>().localPosition = spawnPosition;
     }
 }
